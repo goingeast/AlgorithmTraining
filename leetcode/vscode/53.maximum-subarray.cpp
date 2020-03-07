f0e88ec0b0b4a3774da52ab7f6eb85eaf4c15729
goingeast <holycat188@gmail.com>
2020-03-07 15:15:54 -0800

    add code

--- leetcode/vscode/53.maximum-subarray.cpp
+++ leetcode/vscode/53.maximum-subarray.cpp
@@ -6,7 +6,17 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        //dp[i] 以 i 结尾的最大和
+        // dp[i] = max(nums[i], dp[i-1]+nums[i]);
+        vector<int> dp(nums.size(), 0);
         
+        dp[0] = nums[0];
+        int maxSum = dp[0];
+        for (int i = 1; i < nums.size(); ++i){
+            dp[i] = max(dp[i-1], 0) + nums[i];
+            maxSum = max(maxSum, dp[i]);
+        }
+        return maxSum;
     }
 };
 
